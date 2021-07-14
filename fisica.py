6735fcaa2b62c8e7958fd73eefcdcdabaf155037
Ralfersin <ivolucasandrade@gmail.com>
2021-07-14 12:15:18 -0300

    Adicionado Print removendo Libs

--- fisica.py
+++ fisica.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from seaborn.regression import _RegressionPlotter 
+
 
 tabela = pd.read_excel('trabalhofisicaexcel.xlsx')
+
+print("Olá mundo!!")
+
 
