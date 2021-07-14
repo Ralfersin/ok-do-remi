cdf7249bd9eed32415fe53f4746752e4107ef53d
Alm3iDa <marcus.almeidaif@gmail.com>
2021-07-14 12:41:56 -0300

    teste de teclado e adicionar pausa no script

--- AutoRemi.py
+++ AutoRemi.py
@@ -1,17 +1,21 @@
 import pyautogui as auto
 import time
-import pyperclip as clip
+import pyperclip as pcp
 
 auto.PAUSE = 1
 
 def autoRemi():
    text = 'Oi.'
    auto.click(x=1116, y=634)
-   clip.copy(text)
+   pcp.copy(text)
    auto.hotkey('ctrl','v')
    auto.press('enter')
 
-auto.alert(text = 'O respondedor de chat do Remi tumático vai começar.')
+auto.alert('O respondedor de chat do Remi automático vai começar')
+
 while True:
    autoRemi()
    time.sleep(600)
+   if auto.press('backspace') == 1:
+      break
+
